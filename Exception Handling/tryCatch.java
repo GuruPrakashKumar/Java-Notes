4d63f090da12ef3dd21cf35167e1e108c10136cd
Guru Prakash Kumar <103445950+GuruPrakashKumar@users.noreply.github.com>
2022-12-14 17:36:02 +0530

    Update tryCatch.java

--- Exception Handling/tryCatch.java
+++ Exception Handling/tryCatch.java
@@ -2,9 +2,22 @@
  *-> Exceptions in Java
  *  An exception is an event that occurs when a program is executed dissented the normal flow of instructions.
     There are mainly two types of exceptions in java:
-
+    1)Built-in Exceptions 
+        *Checked Exceptions
+        *Unchecked Exceptions
+    2)User-Defined Exceptions
+    
+    Built-in exceptions are the exceptions that are available in Java libraries.
+    These exceptions are suitable to explain certain error situations.
+    
     1) Checked exceptions - compile-time exceptions (Handle by the compiler)
+       Checked Exceptions: Checked exceptions are called compile-time exceptions because these exceptions are checked at compile-time by the compiler.
     2) Unchecked exceptions - Runtime exceptions
+       The unchecked exceptions are just opposite to the checked exceptions. 
+       The compiler will not check these exceptions at compile time. 
+       In simple words, if a program throws an unchecked exception, and even if we didn’t handle or declare it, the program would not give a compilation error.
+    
+    Check GFG for more information
     
  *  Commonly Occurring Exceptions
  *  Following are few commonly occurring exceptions in java:
